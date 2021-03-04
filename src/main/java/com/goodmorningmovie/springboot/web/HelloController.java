fc75a069547d15ce65adbb92582db0468d826f07
jeon-ji-yoon <appleru1515@gmail.com>
2021-03-04 13:35:45 +0900

    [2021.03.04]엔티티 클래스 추가, save와 findAll 기능 테스트

--- src/main/java/com/goodmorningmovie/springboot/web/HelloController.java
+++ src/main/java/com/goodmorningmovie/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.goodmorningmovie.springboot.web;
 
+import com.goodmorningmovie.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    //가장 최근에 만들엇습니다
+    @GetMapping("/hello/dto")
+    public HelloResponseDto dto(@RequestParam("name") String name,
+                                @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+
     }
 }
---
+++ src/test/java/com/goodmorningmovie/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.goodmorningmovie.springboot.domain.posts;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+//@RunWith(SpringRunner.class) junt5부터는 @SpringBootTest에 포함됨
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach //junit5부터는 aftereach로 변경됨 ㅠㅠ
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("goodmorningmovie").build()   );
+
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+
+}
