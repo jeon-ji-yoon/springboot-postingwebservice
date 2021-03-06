0aed47bd4babc043aadb50530acd791551eec16d
jeon-ji-yoon <appleru1515@gmail.com>
2021-03-07 00:48:29 +0900

    [2021.03.07]게시글save 기능 테스트코드 작성

---
+++ src/test/java/com/goodmorningmovie/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,62 @@
+package com.goodmorningmovie.springboot.web;
+
+
+import com.goodmorningmovie.springboot.domain.posts.Posts;
+import com.goodmorningmovie.springboot.domain.posts.PostsRepository;
+import com.goodmorningmovie.springboot.web.dto.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//가장 마지막에 추가한 한줄의 어노테이션
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception {
+        //given 주어진 상황들을 설정한다
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when 리소스를 해당url에 올리고 응답받은 http의 상태를 체크한다(ex)200, 404... )
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //이건 뭘 검증하는지 모르겟음..assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
