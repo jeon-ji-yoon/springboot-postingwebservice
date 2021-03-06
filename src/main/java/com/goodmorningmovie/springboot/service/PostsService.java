8b03eb5d29f9d94ae1be75d9b7446bc134b85f5c
jeon-ji-yoon <appleru1515@gmail.com>
2021-03-06 15:11:44 +0900

    [2021.03.06]게시글 수정 및 조회기능 개발(1차)

--- src/main/java/com/goodmorningmovie/springboot/service/PostsService.java
+++ src/main/java/com/goodmorningmovie/springboot/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.goodmorningmovie.springboot.service;
 
+import com.goodmorningmovie.springboot.domain.posts.Posts;
 import com.goodmorningmovie.springboot.domain.posts.PostsRepository;
+import com.goodmorningmovie.springboot.web.dto.PostsResponseDto;
 import com.goodmorningmovie.springboot.web.dto.PostsSaveRequestDto;
+import com.goodmorningmovie.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
 
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
