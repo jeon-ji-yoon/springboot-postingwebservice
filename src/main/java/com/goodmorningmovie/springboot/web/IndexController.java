df1d003b50c62cf92c3456ac841068eb52086eb9
jeon-ji-yoon <appleru1515@gmail.com>
2021-03-24 17:32:47 +0900

    [2021.03.24] update 기능 추가

--- src/main/java/com/goodmorningmovie/springboot/web/IndexController.java
+++ src/main/java/com/goodmorningmovie/springboot/web/IndexController.java
@@ -2,10 +2,12 @@ package com.goodmorningmovie.springboot.web;
 
 
 import com.goodmorningmovie.springboot.service.PostsService;
+import com.goodmorningmovie.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor //이건 왜 필요한 어노테이션이지?? 이거 왜 쓰는거?
 @Controller
@@ -30,4 +32,15 @@ public class IndexController {
         //application.properties 에만 두줄 추가 
         //h2 database 웹콘솔을 띄우려고 application.properties에'만' 두줄 추가!!!
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+
+
+
 }
--- src/main/java/com/goodmorningmovie/springboot/web/PostsApiController.java
+++ src/main/java/com/goodmorningmovie/springboot/web/PostsApiController.java
@@ -30,4 +30,6 @@ public class PostsApiController {
 
         return postsService.findById(id);
     }
+
+
 }
